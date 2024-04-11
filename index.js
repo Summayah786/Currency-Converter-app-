a94e820d32b64df2d5c9c682777ae8dc033f74f1
Summayah786 <159770652+Summayah786@users.noreply.github.com>
2024-04-11 10:26:56 +0500

    Add files via upload
    This is a  cli based currency converter

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let currency = {
+    USD: 1, //base currency 
+    EUR: 0.89,
+    GBP: 0.75,
+    INR: 73.84,
+    PKR: 208.00,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    { name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
